98bfb507ce8c98f86b71108b507eb516cf77fde1
rgfie <c10891776@gmail.com>
2017-11-29 14:03:22 -0600

    This program performs the commands required of module m3. All TODO's were converted to DONE.

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Robert Fiedler.
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -69,7 +69,7 @@ dave.forward(200)
 # ----------------------------------------------------------------------
 matt = rg.SimpleTurtle('turtle')
 matt.pen = rg.Pen('red', 30)
-matt.speed = 10  # Faster
+matt.speed = 20  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
@@ -77,7 +77,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +91,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -110,9 +110,22 @@ matt.forward(50)
 #
 ########################################################################
 
+robert = rg.SimpleTurtle()
+robert.pen = rg.Pen('blue', 10)
+robert.right(144)
+robert.forward(100)
+robert.right(144)
+robert.forward(100)
+robert.right(144)
+robert.forward(100)
+robert.right(144)
+robert.forward(100)
+robert.right(144)
+robert.forward(100)
+
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
